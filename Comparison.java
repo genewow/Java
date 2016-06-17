10055f6b24e8901662b7785fa98c8662dffb906c
genewow <genewow@users.noreply.github.com>
2016-06-17 08:23:20 +0800

    Update Comparison.java

--- Comparison.java
+++ Comparison.java
@@ -5,11 +5,11 @@ public class Comparison
     public static void main( String args[] )    
     {
         // create Scanner to obtain input from command window       
-        Scanner input = Scanner( System.in );  
+        Scanner input =new Scanner( System.in );  
         int number1; // first number to compare       
         int number2; // second number to compare  
       
-        System.out.prnt( "Enter first integer: " ); // prompt        
+        System.out.print( "Enter first integer: " ); // prompt        
         number1 = input.nextInt(); // read first number from user   
  
         System.out.print( "Enter second integer: " ); // prompt        
@@ -29,4 +29,4 @@ public class Comparison
             System.out.printf( "%d >= %d\n", number1, number2 );  
     }
    } // end method main  
-} // end class Comparison 
+   // end class Comparison 
